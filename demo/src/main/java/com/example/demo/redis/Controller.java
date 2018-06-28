9562cd0200c69da9edb9755b4cb9dfbbb388d929
ang.li <765021003@qq.com>
2018-06-28 09:40:24 +0800

    init
    init

---
+++ demo/src/main/java/com/example/demo/redis/Controller.java
@@ -0,0 +1,10 @@
+package com.example.demo.redis;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/redis")
+public class Controller {
+
+}
