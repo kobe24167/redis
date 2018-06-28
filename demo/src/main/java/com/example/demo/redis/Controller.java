07d0c95d4190d49106b1a9f4c264e23666b7fbf4
ang.li <765021003@qq.com>
2018-06-28 10:51:31 +0800

    add request
    add request

--- demo/src/main/java/com/example/demo/redis/Controller.java
+++ demo/src/main/java/com/example/demo/redis/Controller.java
@@ -1,10 +1,21 @@
 package com.example.demo.redis;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import redis.clients.jedis.Jedis;
 
 @RestController
 @RequestMapping("/redis")
 public class Controller {
 
+	@RequestMapping(method = RequestMethod.POST)
+	public String getRedisValue (@RequestParam(value = "key") String key) {
+		Jedis jedis = new Jedis("116.196.97.97");
+		String value = jedis.get(key);
+		return value;
+	}
+	
 }
